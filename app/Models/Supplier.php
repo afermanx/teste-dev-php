aa4aac93e3f73c531c396ddbba0ab4580434f012
Alex <alexjferman@gmail.com>
2024-12-15 22:27:59 -0300

    perf: add cache system

--- app/Models/Supplier.php
+++ app/Models/Supplier.php
@@ -2,10 +2,12 @@
 
 namespace App\Models;
 
+use App\Models\User;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+
 
 class Supplier extends Model
 {
